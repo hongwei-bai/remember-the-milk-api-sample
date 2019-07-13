cd371bbd79c3a274902b6b437eca73f07858b789
Mike <bhw8412@hotmail.com>
2019-07-13 18:30:45 +1000

    init project

---
+++ app/src/main/java/com/hongwei/remember_the_milk_api_sample/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.hongwei.remember_the_milk_api_sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
