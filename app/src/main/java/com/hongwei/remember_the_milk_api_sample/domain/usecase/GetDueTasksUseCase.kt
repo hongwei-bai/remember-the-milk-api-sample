06a902cca33b2dee078e9928a7807a8be03166ea
Mike Hongwei Bai <hongwei.bai@outware.com.au>
2019-09-04 16:11:24 +1000

    complete recurrence feature

--- app/src/main/java/com/hongwei/remember_the_milk_api_sample/domain/usecase/GetDueTasksUseCase.kt
+++ app/src/main/java/com/hongwei/remember_the_milk_api_sample/domain/usecase/GetDueTasksUseCase.kt
@@ -4,9 +4,19 @@ import android.util.Log
 import com.hongwei.remember_the_milk_api_sample.ApiConfig.AppString.LIST_ALL_TASKS
 import com.hongwei.remember_the_milk_api_sample.data.DataSource
 import com.hongwei.remember_the_milk_api_sample.domain.model.DueTask
+import com.hongwei.remember_the_milk_api_sample.util.dayDiff
+import com.hongwei.remember_the_milk_api_sample.util.monthDiff
 import com.hongwei.remember_the_milk_api_sample.util.now
 import com.hongwei.remember_the_milk_api_sample.util.tomorrow
+import com.hongwei.remember_the_milk_api_sample.util.weekDiff
+import com.hongwei.remember_the_milk_api_sample.util.yearDiff
+import it.bova.rtmapi.Frequency.DAILY
+import it.bova.rtmapi.Frequency.MONTHLY
+import it.bova.rtmapi.Frequency.WEEKLY
+import it.bova.rtmapi.Frequency.YEARLY
 import it.bova.rtmapi.RtmApi
+import it.bova.rtmapi.Task
+import java.util.Date
 import javax.inject.Inject
 
 class GetDueTasksUseCase @Inject constructor(val dataSource: DataSource) {
@@ -42,21 +52,31 @@ class GetDueTasksUseCase @Inject constructor(val dataSource: DataSource) {
                 val taskProgress = listProgress + (listProgressNt - listProgress) * j / tasks.size
                 progressNotify.invoke(taskProgress)
 
-                Log.i("aaaa", "task: ${task.name}, due: ${task.due}, recurrence: ${task.recurrence}")
-
                 if ((task.completed != null && task.completed < now())
                     || (task.deleted != null && task.deleted < now())
                 ) {
                     continue
                 }
 
-                var dueRecurrence = false
-                if (task.recurrence != null) {
-
-                }
-
-                task.due?.let { taskDueDate ->
-                    if (taskDueDate.after(now()) && taskDueDate.before(tomorrow())) {
+                task.due?.apply {
+                    if (task.recurrence != null) {
+                        getRecurrenceDue(task)?.let { dueRecurrence ->
+                            dueRecurrence.apply {
+                                hours = 9
+                                if (after(now())) {
+                                    Log.i(TAG, "[RECURRENCE MOR]task: ${task.name}, due: ${this}")
+                                    dueTaskList.add(DueTask(task.name, this))
+                                }
+                            }
+                            dueRecurrence.apply {
+                                hours = 19
+                                if (after(now())) {
+                                    Log.i(TAG, "[RECURRENCE MOR]task: ${task.name}, due: ${this}")
+                                    dueTaskList.add(DueTask(task.name, this))
+                                }
+                            }
+                        }
+                    } else if (after(now()) && before(tomorrow())) {
                         Log.i(TAG, "[TODAY]task: ${task.name}, due: ${task.due}")
                         dueTaskList.add(DueTask(task.name, task.due))
                     }
@@ -67,5 +87,53 @@ class GetDueTasksUseCase @Inject constructor(val dataSource: DataSource) {
         }
         Log.i(TAG, "-- end --")
         return dueTaskList
+    }
+
+    private fun getRecurrenceDue(task: Task): Date? {
+        if (null == task.recurrence) {
+            return null
+        }
+
+        if (!task.recurrence.isEvery) {
+            return null
+        }
+
+        var isToday = false
+        when (task.recurrence.frequency) {
+            DAILY -> {
+                if (now().dayDiff(task.due) % task.recurrence.interval == 0L) {
+                    isToday = true
+                }
+            }
+            WEEKLY -> {
+                val weekDiff = now().weekDiff(task.due)
+                if (weekDiff != null && weekDiff % task.recurrence.interval == 0) {
+                    isToday = true
+                }
+            }
+            MONTHLY -> {
+                val monthDiff = now().monthDiff(task.due)
+                if (monthDiff != null && monthDiff % task.recurrence.interval == 0) {
+                    isToday = true
+                }
+            }
+            YEARLY -> {
+                val yearDiff = now().yearDiff(task.due)
+                if (yearDiff != null && yearDiff % task.recurrence.interval == 0) {
+                    isToday = true
+                }
+            }
+            else -> {
+            }
+        }
+
+        if (isToday) {
+            return Date().apply {
+                hours = task.due.hours
+                minutes = task.due.minutes
+                seconds = task.due.seconds
+            }
+        }
+        return null
     }
 }
--- app/src/main/java/com/hongwei/remember_the_milk_api_sample/util/DateUtil.kt
+++ app/src/main/java/com/hongwei/remember_the_milk_api_sample/util/DateUtil.kt
@@ -12,3 +12,39 @@ fun tomorrow() = Date(today().time + (1000 * 60 * 60 * 24))
 
 fun tomorrow(hour: Int) = tomorrow().let { Date(it.year, it.month, it.date, hour, 0, 0) }
 
+private val MS_IN_DAY = 1000.0 * 3600 * 24
+
+fun Date.dayDiff(ref: Date) = Math.round((this.apply {
+    hours = 2
+    minutes = 0
+    seconds = 0
+}.time - ref.apply {
+    hours = 2
+    minutes = 0
+    seconds = 0
+}.time) / MS_IN_DAY)
+
+fun Date.weekDiff(ref: Date) = if (this.day == ref.day) {
+    val days = this.dayDiff(ref)
+    if (days % 7 == 0L) (days / 7).toInt() else null
+} else null
+
+fun Date.monthDiff(ref: Date) = if (this.date == ref.date) {
+    (this.year - ref.year) * 12 + this.month - ref.month
+} else null
+
+fun Date.yearDiff(ref: Date) = if (this.date == ref.date && this.month == ref.month) {
+    this.year - ref.year
+} else null
+
+fun Date.dayStart() = this.apply {
+    hours = 0
+    minutes = 0
+    seconds = 0
+}
+
+fun Date.dayEnd() = this.apply {
+    hours = 23
+    minutes = 59
+    seconds = 59
+}
